7f8a24796c9321f2b8e5f4bc4d214191ac26e772
Nick Johnson <nick@yonson.dev>
2026-04-02 13:31:55 -0700

    cargo-rbmt: upgrade benches output

--- cargo-rbmt/src/bench.rs
+++ cargo-rbmt/src/bench.rs
@@ -2,14 +2,13 @@
 
 use xshell::Shell;
 
-use crate::environment::{rbmt_eprintln, Package};
+use crate::environment::{rbmt_eprintln, OutputMode, Package};
 use crate::rbmt_cmd;
 use crate::toolchain::{prepare_toolchain, Toolchain};
 
 /// Run benchmark tests for all crates in the workspace.
 pub fn run(sh: &Shell, packages: &[Package]) -> Result<(), Box<dyn std::error::Error>> {
     prepare_toolchain(sh, Toolchain::Nightly)?;
-
     rbmt_eprintln(&format!("Running bench tests for {} crates", packages.len()));
 
     for package in packages {
@@ -18,8 +17,14 @@ pub fn run(sh: &Shell, packages: &[Package]) -> Result<(), Box<dyn std::error::E
         // Use pushd pattern to change and restore directory.
         let _dir = sh.push_dir(&package.dir);
 
-        rbmt_cmd!(sh, "cargo --locked bench").env("RUSTFLAGS", "--cfg=bench").run()?;
+        // Capture output and show in stdout for verbose mode.
+        let output =
+            rbmt_cmd!(sh, "cargo --locked bench").env("RUSTFLAGS", "--cfg=bench").read()?;
+        if matches!(OutputMode::from_env(), OutputMode::Verbose) {
+            println!("{}", output);
+        }
     }
 
+    rbmt_eprintln("Benches complete.");
     Ok(())
 }
